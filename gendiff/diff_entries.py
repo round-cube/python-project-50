fdcc049c11d77010055545ddc169deab05f62ce5
Gleb Voropaev <info@gvoropaev.com>
2022-12-28 17:28:31 +0200

    Refactored generate_diff:
    - no more classes in the codebase
    - no more function definitions in __init__.py

--- gendiff/diff_entries.py
+++ gendiff/diff_entries.py
@@ -1,95 +1,72 @@
 """Internal diff representation."""
 
 
-class DiffEntryType:
-    pass
-
-
-class DiffEntriesGenerator:
-    def __init__(self, source, compared_to):
-        self.source = source
-        self.compared_to = compared_to
-        self._diff_entries = []
-
-    def _get_formatted_property(self, v, key):
-        """Get formatted property."""
-        value = v[key]
-        formatted_value = self._format_value(value)
-        formatted_propery = {"key": key, "value": formatted_value}
-        return formatted_propery
-
-    def _format_value(self, v):
-        """Recursively format value."""
-        if type(v) is dict:
-            properties = []
-            for key in v:
-                property = self._get_formatted_property(v, key)
-                properties.append(property)
-            return properties
+def _format_value(v):
+    """Recursively format value."""
+    if type(v) is not dict:
         return v
+    properties = []
+    for key in v:
+        value = v[key]
+        formatted_value = _format_value(value)
+        property = {"key": key, "value": formatted_value}
+        properties.append(property)
+    return properties
 
-    @classmethod
-    def _get_nested_entry(cls, key, source_value, compared_value):
-        diff_generator = cls(source_value, compared_value)
-        formatted_value = diff_generator.get_entries()
-        return {"key": key, "value": formatted_value}
 
-    def _get_added_entry(self, key, value):
-        return {"type": "added", "key": key, "value": self._format_value(value)}
+def _get_new_key_diff_entries(source, compared_to):
+    """Process keys that are contained only in self.compared_to"""
+    source_keys = set(source.keys())
+    compared_keys = set(compared_to.keys())
+    for key in compared_keys - source_keys:
+        value = compared_to[key]
+        yield {"type": "added", "key": key, "value": _format_value(value)}
 
-    def _get_unchanged_entry(self, key, value):
-        formatted_value = self._format_value(value)
-        return {"key": key, "value": formatted_value}
 
-    def _get_updated_entry(self, key, old_value, new_value):
-        formatted_old_value = self._format_value(old_value)
-        formatted_new_value = self._format_value(new_value)
+def _get_commmon_key_diff_entry(source_key, source_value, compared_to):
+    """Generate formatted key diff entry."""
+
+    if source_key not in compared_to.keys():
         return {
-            "type": "updated",
-            "key": key,
-            "old_value": formatted_old_value,
-            "new_value": formatted_new_value,
+            "type": "removed",
+            "key": source_key,
+            "value": _format_value(source_value),
         }
 
-    def _get_removed_entry(self, key, value):
-        formatted_value = self._format_value(value)
-        return {"type": "removed", "key": key, "value": formatted_value}
+    compared_value = compared_to[source_key]
+    types_are_equal = type(source_value) == type(compared_value) == dict
+    values_are_different = source_value != compared_value
 
-    def _process_new_keys(self):
-        """Process keys that are contained only in self.compared_to"""
-        source_keys = set(self.source.keys())
-        compared_keys = set(self.compared_to.keys())
-        for key in compared_keys - source_keys:
-            value = self.compared_to[key]
-            entry = self._get_added_entry(key, value)
-            self._diff_entries.append(entry)
+    if types_are_equal and values_are_different:
+        return {
+            "key": source_key,
+            "value": get_diff_entries(source_value, compared_value),
+        }
+    elif source_value == compared_value:
+        return {"key": source_key, "value": _format_value(source_value)}
+    else:
+        return {
+            "type": "updated",
+            "key": source_key,
+            "old_value": _format_value(source_value),
+            "new_value": _format_value(compared_value),
+        }
 
-    def _process_commmon_key(self, source_key, source_value):
-        if source_key in self.compared_to.keys():
-            compared_value = self.compared_to[source_key]
-            types_are_equal = type(source_value) == type(compared_value) == dict
-            values_are_different = source_value != compared_value
-            if types_are_equal and values_are_different:
-                entry = self._get_nested_entry(
-                    source_key, source_value, compared_value
-                )
-            elif source_value == compared_value:
-                entry = self._get_unchanged_entry(source_key, source_value)
-            else:
-                entry = self._get_updated_entry(
-                    source_key, source_value, compared_value
-                )
-        else:
-            entry = self._get_removed_entry(source_key, source_value)
-        self._diff_entries.append(entry)
 
-    def get_entries(self):
-        """Generate diff entries between source and compared_to dicts."""
-        for source_key in self.source:
-            source_value = self.source[source_key]
-            self._process_commmon_key(source_key, source_value)
-        self._process_new_keys()
-        return self._diff_entries
+def get_diff_entries(source, compared_to):
+    """Generate diff entries between source and compared_to dicts."""
+    diff_entries = []
+
+    for source_key in source:
+        source_value = source[source_key]
+        entry = _get_commmon_key_diff_entry(
+            source_key, source_value, compared_to
+        )
+        diff_entries.append(entry)
+
+    for diff_entry in _get_new_key_diff_entries(source, compared_to):
+        diff_entries.append(diff_entry)
+    return diff_entries
 
 
 def sort_by_key(entries):
---
+++ gendiff/generate_diff.py
@@ -0,0 +1,24 @@
+from gendiff.files import open_file
+from gendiff.diff_entries import get_diff_entries, sort_by_key
+from gendiff.formatters import stylish
+from gendiff.formatters import plain
+from gendiff.formatters import json
+
+SUPPORTED_FORMATTERS = ["stylish", "plain", "json"]
+SUPPORTED_FORMATTERS_EXC = f"Supported formatters: {SUPPORTED_FORMATTERS}."
+
+
+def generate_diff(file_path1, file_path2, formatter="stylish"):
+    source, compared_to = open_file(file_path1), open_file(file_path2)
+    diff_entries = get_diff_entries(source, compared_to)
+    sorted_diff_entries = sort_by_key(diff_entries)
+    if formatter == "stylish":
+        formatter = stylish
+    elif formatter == "plain":
+        formatter = plain
+    elif formatter == "json":
+        formatter = json
+    else:
+        raise ValueError(SUPPORTED_FORMATTERS_EXC)
+
+    return formatter.format_diff_entries(sorted_diff_entries)
